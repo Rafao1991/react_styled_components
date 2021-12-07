70516a5d7fa317f82c37ec376e3dd0de6fc01a54
Rafael Sousa (Rafao) <1c302489@gmail.com>
2021-12-06 21:05:51 -0300

    styled components implemented

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+
+import { ThemeProvider } from "styled-components";
+import { temaClaro, temaEscuro } from "./Components/UI/temas";
 
 import Cabecalho from "./Components/Cabecalho";
 import Container from "./Components/Container";
+import SwitchTema from "./Components/SwitchTema";
+
 import { GlobalStyle } from "./Components/GlobalStyle";
+import { BtnTema } from "./Components/UI";
 
 function App() {
+  const [tema, setTema] = useState(true);
+
+  const toggleTema = () => {
+    setTema(!tema)
+  };
+
   return (
-    <>
+    <ThemeProvider theme={tema ? temaClaro : temaEscuro}>
       <GlobalStyle />
+      <BtnTema onClick={toggleTema}>
+        <SwitchTema tema={tema}/>
+      </BtnTema>
       <Cabecalho />
       <Container />
-    </>
+    </ThemeProvider>
   );
 }
 
--- src/Components/Container/index.jsx
+++ src/Components/Container/index.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import Titulo from "../Titulo";
 import Conta from "../Conta";
 import styled from "styled-components";
+import Extrato from "../Extrato";
 
 const Container = styled.div`
-  background-color: #f1f1f1;
+  background-color: ${ ({theme}) => theme.body };
   min-height: 90vh;
   padding: 0px 15vw;
 `;
 
 const Conteudo = styled.section`
+  color: ${ ({theme}) => theme.text };
   display: flex;
   flex-direction: row;
   justify-content: space-between;
@@ -25,6 +27,7 @@ export default () => {
       <Titulo>Olá Fulano!</Titulo>
       <Conteudo>
         <Conta />
+        <Extrato />
       </Conteudo>
     </Container>
   );
